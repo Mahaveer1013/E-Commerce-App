fe8e0915900c389fb2b2f12e83a0ca5b45e54376
Mahaveer1013 <135404390+Mahaveer1013@users.noreply.github.com>
2024-08-14 19:54:37 +0530

    firebase config for backend

--- backend/src/firebaseAdmin.js
+++ backend/src/firebaseAdmin.js
@@ -1,10 +1,20 @@
 import admin from 'firebase-admin';
-import { readFileSync } from 'fs';
-import path from 'path';
+import dotenv from 'dotenv'
+dotenv.config()
 
-// Resolve path to your JSON file
-const serviceAccountPath = path.resolve('./event-db-393fb-firebase-adminsdk-92cxv-6d3d050954.json');
-const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, 'utf8'));
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Replace escaped newlines with actual newlines
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN
+  };
 
 // Initialize Firebase Admin SDK
 admin.initializeApp({
