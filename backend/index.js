67a667b37886229ff0ea1923a28f6fa50d2fe216
Mahaveer1013 <135404390+Mahaveer1013@users.noreply.github.com>
2024-08-15 09:47:31 +0530

    deploymeny changes

--- backend/index.js
+++ backend/index.js
@@ -15,11 +15,10 @@ app.use(encryptResponse);
 app.use(decryptRequest);
 
 
-const allowedOrigins = ['https://mahaveer-e-commerce.netlify.app', 'http://localhost:5173', 'https://localhost:3000'];
+const allowedOrigins = ['https://mahaveer-e-commerce.netlify.app','https://mahaveer-e-commerce.netlify.app/','https://mahaveer-e-commerce.netlify.app/*', 'http://localhost:5173/', 'https://localhost:3000/'];
 
 app.use(cors({
   origin: function (origin, callback) {
-    // Allow requests with no origin like mobile apps or curl requests
     if (!origin) return callback(null, true);
     if (allowedOrigins.indexOf(origin) === -1) {
       const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
