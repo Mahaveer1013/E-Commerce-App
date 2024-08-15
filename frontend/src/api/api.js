a6f9f785f0eb9fd4e53391fb5b1dfc00dfe82c14
Mahaveer1013 <135404390+Mahaveer1013@users.noreply.github.com>
2024-08-15 10:30:47 +0530

    client side change for deploy

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,10 +2,16 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL,
-    withCredentials: true, 
+    withCredentials: true,
 });
 
-console.log('My backend url is '+ import.meta.env.VITE_BACKEND_URL, '\n\n');
+console.log('My backend url is ' + import.meta.env.VITE_BACKEND_URL, '\n\n');
 
+try {
+    const response = api.get('/check')
+    console.log(response.data);
+} catch (error) {
+    console.log(error);
+}
 
 export default api;
