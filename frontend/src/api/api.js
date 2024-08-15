f513fe9cbe1917d2b0cf5b1e78618829cce5192f
Mahaveer1013 <135404390+Mahaveer1013@users.noreply.github.com>
2024-08-15 10:40:30 +0530

    wecdfv

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -8,7 +8,7 @@ const api = axios.create({
 console.log('My backend url is ' + import.meta.env.VITE_BACKEND_URL, '\n\n');
 
 try {
-    const response = api.get('/check')
+    const response = api.get('/')
     console.log(response.data);
 } catch (error) {
     console.log(error);
