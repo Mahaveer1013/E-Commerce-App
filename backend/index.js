384577a68c0482cdecf6c3b05b355a31b9964f20
Mahaveer1013 <135404390+Mahaveer1013@users.noreply.github.com>
2024-08-15 09:43:48 +0530

    deployment changes

--- backend/index.js
+++ backend/index.js
@@ -15,10 +15,26 @@ app.use(encryptResponse);
 app.use(decryptRequest);
 
 
+const allowedOrigins = ['https://mahaveer-e-commerce.netlify.app', 'http://localhost:5173', 'https://localhost:3000'];
+
 app.use(cors({
-  origin: '*',
+  origin: function (origin, callback) {
+    // Allow requests with no origin like mobile apps or curl requests
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   credentials: true
 }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 
 app.use(cookieParser());
 
--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -4,14 +4,3 @@ const api = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL,
     withCredentials: true, 
 });
-
-api.interceptors.request.use((config) => {
-    if (config.data) {
-        console.log(config);
-    }
-    return config;
-}, (error) => {
-    return Promise.reject(error);
-});
-
-export default api;
